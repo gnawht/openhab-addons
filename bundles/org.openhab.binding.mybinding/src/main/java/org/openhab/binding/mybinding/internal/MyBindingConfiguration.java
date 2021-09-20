a3754d9bb9cad825a0d135839c65a156c748ca4a
Thomas Wang-NIelsen <thomas@wang-nielsen.dk>
2021-09-20 11:42:43 +0200

    After binding creation

---
+++ bundles/org.openhab.binding.mybinding/src/main/java/org/openhab/binding/mybinding/internal/MyBindingConfiguration.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.mybinding.internal;
+
+/**
+ * The {@link MyBindingConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Thomas Wang-Nielsen - Initial contribution
+ */
+public class MyBindingConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String hostname;
+    public String password;
+    public int refreshInterval;
+}
